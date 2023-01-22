104b4e5b627f2d9cb08eaaf7425ce20bb6a298de
tom <t_hankinson@outlook.com>
2023-01-22 16:56:38 +0000

    Added model to the description page to display movie synopsis

---
+++ components/ui/ModalButton.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text } from "react-native";
+
+function ModalButton({children, onPress}){
+  return (
+
+      <Text onPress={onPress} style={styles.text} >
+          {children} 
+      </Text>
+   
+  )
+};
+
+export default ModalButton;
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'blue',
+    fontStyle: 'italic',
+    underline: {textDecorationLine: 'underline'}
+  }
+})
